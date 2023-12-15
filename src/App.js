fcffbeb0f8e90e67e9898c118fef7bc7c652ec3e
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-15 11:50:10 +0530

    header section done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 
-import Body from './Body/Body';
-import Footer from './Footer/Footer';
+
 import Header from './Header/Header';
 
 function App() {
   return (
-    <div className="p-4">
+    <>
       <Header />
-      <Body />
-      <Footer/>
-    </div>
+    </>
   );
 }
 
