19968115f0f2fd78af5967fcb368b685a384a745
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-27 16:31:19 +0530

    restarant maping done

--- src/Components/Restaurant/RestaurantList.js
+++ src/Components/Restaurant/RestaurantList.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard'
+import Restaurants from '../../Data/Restaurants.json'
 
 const RestaurantList = () => {
   return (
-      <div className="max-w-screen-xl mx-auto px-4 md:px-6 gap-6 my-8 ">
-        <RestaurantCard />;
-       
+    <div className="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 px-4 md:px-6 gap-6 my-8 ">
+      
+      {Restaurants.map(data => {
+        // console.log(data);
+        return <RestaurantCard key={data._id.$oid} data={data} />
+      })}
+        
       </div>
 
     
